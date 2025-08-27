d4a7a23ac545c071fe4e0ab43246af75b04a70a2
sardor2009css <sardortoshtemirov621@gmail.com>
2025-08-27 21:41:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let ism = prompt ("Ismingizni kiriting")
+let age = prompt("Yoshingizni kiritng")
+let surname = prompt("Familyangizni kiriting ")
+
+document.getElementById("malumot").innerText = "Sizning ismingiz: " +ism
+
+document.getElementById("age").innerText = "Sizning yoshingiz: " +age
+
+document.getElementById("surname").innerText = "Sizning familyangiz: " +surname
